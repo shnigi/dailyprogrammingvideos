abf0682772d47b4461ccd474862a9d008c8e067c
Niki Ahlskog <niki.ahlskog@gmail.com>
2018-07-19 13:50:09 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const videoList = document.querySelector('.videolist');
+const videoChunks = _.chunk(videos, 5);
+const lastChunk = _.lastIndexOf(videoChunks);
+const navButton = document.querySelector('.toggleNav');
+const flexNav = document.querySelector('.flex-nav ul');
+const menuButtons = document.querySelectorAll('.menu-link');
+
+const loopVideos = (chunk) => {
+  return videoChunks[chunk].map(({url, name}) =>
+  `
+    <h2>${name}</h2>
+    <div class="embed-container">
+      <iframe width="560" height="315"
+       src="${url}"
+       frameborder="0" encrypted-media" allowfullscreen>
+      </iframe>
+    </div>
+  `);
+};
+
+const showVideos = (chunk) => {
+  videoList.innerHTML = loopVideos(chunk).join('');
+};
+
+showVideos(0);
+
+navButton.addEventListener('click', () => {
+  flexNav.classList.toggle("open");
+});
+
+let chunk = 0;
+function menuFunction () {
+  if (this.dataset.page === 'next') {
+    if (chunk < (videoChunks.length - 1)) {
+      chunk++;
+    }
+  } else if (this.dataset.page === 'previous') {
+    if (chunk !== 0) {
+      chunk--;
+    }
+  } else if (this.dataset.page === 'first') {
+    chunk = 0;
+  } else if (this.dataset.page === 'last') {
+    chunk = lastChunk - 1;
+  }
+  if (this.dataset.bottomlink) {
+    window.scrollTo(0, 0);
+  }
+  showVideos(chunk);
+};
+
+menuButtons.forEach(button => button.addEventListener('click', menuFunction));
---
+++ videos.js
@@ -0,0 +1,31 @@
+const videos = [{
+  url: 'https://www.youtube.com/embed/zYt0WbDjJ4E',
+  name: 'When you complete a neat feature and client gives you feedback'
+},{
+  url: 'https://www.youtube.com/embed/oXYZttxunPw',
+  name: 'When your code doesn\'t work and you struggle to find the reason'
+}, {
+  url: 'https://www.youtube.com/embed/mqBPlqQOgis',
+  name: 'When all is going smoothly and then you hit an error'
+},
+{
+  url: 'https://www.youtube.com/embed/H1aRbQ6zUjs',
+  name: 'When you finally thought the product is ready but then project manager decides the app needs new features'
+},
+{
+  url: 'https://www.youtube.com/embed/5FjWe31S_0g',
+  name: 'When the project is finally finished and contract at client ended'
+},
+{
+  url: 'https://www.youtube.com/embed/JbpR3i1rIAs',
+  name: 'Code is shit and broken and everything is out of your hands'
+},
+{
+  url: 'https://www.youtube.com/embed/QH2-TGUlwu4',
+  name: 'You got the flow and app works like a charm. Just smooth riding'
+},
+{
+  url: 'https://www.youtube.com/embed/lwoBfLTZxRE',
+  name: 'Project manager requests a new impossible feature'
+}
+];
