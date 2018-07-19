65f5e4b147f680a73d010c4a64e308727122591f
Niki Ahlskog <niki.ahlskog@gmail.com>
2018-07-19 14:12:26 +0300

    pagenumber

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const lastChunk = _.lastIndexOf(videoChunks);
 const navButton = document.querySelector('.toggleNav');
 const flexNav = document.querySelector('.flex-nav ul');
 const menuButtons = document.querySelectorAll('.menu-link');
+const pageNumber = document.querySelector('.page-number');
+let chunk = 0;
 
 const loopVideos = (chunk) =>
 videoChunks[chunk].map(({url, name}) =>
@@ -27,24 +29,32 @@ navButton.addEventListener('click', () => {
   flexNav.classList.toggle("open");
 });
 
-let chunk = 0;
+const showPageNumber = () => {
+  pageNumber.textContent = `Page: ${chunk + 1} / ${videoChunks.length}`;
+};
+showPageNumber();
+
 function menuFunction () {
   if (this.dataset.page === 'next') {
     if (chunk < (videoChunks.length - 1)) {
       chunk++;
       showVideos(chunk);
+      showPageNumber();
     }
   } else if (this.dataset.page === 'previous') {
     if (chunk !== 0) {
       chunk--;
       showVideos(chunk);
+      showPageNumber();
     }
   } else if (this.dataset.page === 'first') {
     chunk = 0;
     showVideos(chunk);
+    showPageNumber();
   } else if (this.dataset.page === 'last') {
     chunk = lastChunk - 1;
     showVideos(chunk);
+    showPageNumber();
   }
   if (this.dataset.bottomlink) {
     window.scrollTo(0, 0);
